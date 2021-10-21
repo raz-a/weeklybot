95a23e110dbce2f55b2d2677f918563b44b68ffe
Raz <raz-@outlook.com>
2021-10-20 22:17:28 -0700

    woop

---
+++ app.js
@@ -0,0 +1,35 @@
+const tmi = require("tmi.js");
+
+var channels = ["naircat", "razstrats"];
+
+const opts = {
+    identity: {
+        username: "weeklywednesdaybot",
+        password: "oauth:pdedycnpfqlxs6dnbyqcsb4lbm195l"
+    },
+    channels: channels
+};
+
+
+const client = new tmi.client(opts);
+
+client.on("connected", onConnectedHandler);
+client.on("message", onMessageHandler);
+
+client.connect();
+
+function onMessageHandler(target, context, msg, self) {
+    if (self) {
+        return;
+    }
+
+    for (const channel of channels) {
+        if (channel !== target) {
+            client.say(channel, `[${target}'s chat] ${context.username}: ${msg}`);
+        }
+    }
+}
+
+function onConnectedHandler(addr, port) {
+    console.log(`* Connected to ${addr}:${port}`);
+}
