a0e82c4037251d3d3529d4e6af47aae343017f99
Raz <raz-@outlook.com>
2021-12-08 20:16:04 -0800

    first

--- app.js
+++ app.js
@@ -1,35 +1,79 @@
 const tmi = require("tmi.js");
 
-var channels = ["naircat", "razstrats"];
+// Define channels to connect to.
+const channels = ["razstrats", "naircat"];
 
+// Define the bot's login info.
 const opts = {
     identity: {
-        username: "weeklywednesdaybot",
-        password: "oauth:pdedycnpfqlxs6dnbyqcsb4lbm195l"
-    },
-    channels: channels
+        username: "weekly_bot",
+        password: "oauth:pv8vf6m4mgsqs378wvup9z4n88vfho"
+    }
 };
 
-
+// Create the client object.
 const client = new tmi.client(opts);
 
-client.on("connected", onConnectedHandler);
+// Register the message handler.
 client.on("message", onMessageHandler);
 
-client.connect();
+// Connect to the twitch server.
+client.connect().then(() => {
 
-function onMessageHandler(target, context, msg, self) {
+    // Attempt to join each channel.
+    for (const channel of channels) {
+        client.join(channel).catch((err) => {
+            console.log(`ERROR: ${err}`);
+            process.exit(1);
+        });
+    }
+
+    // Set the color.
+    client.color("SpringGreen");
+}).catch((err) => {
+    console.log(`ERROR: ${err}`);
+    process.exit(1);
+});
+
+function onMessageHandler(target, user, msg, self) {
+
+    // Ignore messages from self.
     if (self) {
         return;
     }
 
-    for (const channel of channels) {
+    // Check for benis....
+    if (msg.toLowerCase().includes("benis")) {
+        console.log("b*nis detected");
+        client.say(target, `Yo ${user["display-name"]}. What the fuck is wrong with you?`);
+        if ((user.mod === false) && (user.username !== target.slice(1))) {
+            client.timeout(target, user.username, 10, "Bro you can't say that shit here").catch((err) => {
+                console.log(`ERROR: ${err}`)
+            });
+        }
+
+        return;
+    }
+
+    // Broadcast to all other channels.
+    for (const channel of client.getChannels()) {
         if (channel !== target) {
-            client.say(channel, `[${target}'s chat] ${context["display-name"]}: ${msg}`);
+            console.log(`Sending message from ${target} to ${channel}`);
+            switch (user["message-type"]) {
+                case "action":
+                    client.action(channel, `【${user["display-name"]}】 ${msg}`).catch((err) => {
+                        console.log(`ERROR: ${err}`);
+                    });
+
+                    break;
+
+                case "chat":
+                    client.say(channel, `【${user["display-name"]}】 ${msg}`).catch((err) => {
+                        console.log(`ERROR: ${err}`);
+                    });
+
+                    break;
+            }
         }
     }
-}
-
-function onConnectedHandler(addr, port) {
-    console.log(`* Connected to ${addr}:${port}`);
 }
