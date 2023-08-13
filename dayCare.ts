1c344a70c1f5c266222d5d2044bb6674d25bc4db
Jade Shackelford <j.shackelford@g.austincc.edu>
2023-08-12 22:35:44 -0500

    added dayCare class

--- dayCare.ts
+++ dayCare.ts
@@ -16,7 +16,7 @@ export type Child = { //child stats, generated upon birth
     father: string;
     birthDate: Date; //for use in birthday command
 
-    //apgar stats are for baby-fighting command
+    //apgar stats are for baby-fighting 
     appearance: number; //health
     pulse: number; //health recovery
     grimace: number; //energy
@@ -41,7 +41,12 @@ export abstract class Pregnancy {
         if (baby != undefined) {
             let date: Date = new Date();
             if (date.getTime() > baby.conceptionDate.getTime() + 1814400000) { //3 weeks in miliseconds
-                let appearance = randomInt(1, 10), pulse = randomInt(1, 10), grimace = randomInt(1, 10), activity = randomInt(1, 10), respiration = randomInt(1, 10);
+                let appearance = 0, pulse = 0, grimace = 0, activity = 0, respiration = 0;
+                if (baby.mother === baby.father) {
+                    let appearance = randomInt(1, 5), pulse = randomInt(1, 5), grimace = randomInt(1, 5), activity = randomInt(1, 5), respiration = randomInt(1, 5);
+                } else {
+                    let appearance = randomInt(1, 10), pulse = randomInt(1, 10), grimace = randomInt(1, 10), activity = randomInt(1, 10), respiration = randomInt(1, 10);
+                }
                 let child = <Child>{name:babyName, mother:baby.mother, father:baby.father, birthDate:date, appearance:appearance, pulse:pulse, grimace:grimace, activity:activity, respiration:respiration};
                 await this.#db.push(this.#child_key, child);
                 this.abortion(userName);
@@ -55,9 +60,7 @@ export abstract class Pregnancy {
             if (baby) {
                 await this.#db.delete(this.#baby_key + `[/${baby.mother}]`);
             }
-        } catch (err) {
-            console.error("Error during abortion:", err);
-        }
+        } catch (err) {}
     }
 
     static async checkPregnancy(userName: string): Promise<Readonly<Baby> | undefined> {
@@ -75,6 +78,31 @@ export abstract class Pregnancy {
         }
     }
 
-    
+
     //CHILD FUNCTIONALITY
+    static async listChildren(userName: string): Promise<string> {
+        try {
+            const children = await this.#db.getData(this.#child_key);
+            const childNames: string[] = [];
+            
+            for (const child of children) {
+                if (child.mother === userName || child.father === userName) {
+                    childNames.push(child.name);
+                }
+            }
+            
+            return childNames.join(', ');
+        } catch (err) {
+            return "";
+        }
+    }
+
+    static async getChild(childName: string): Promise<Child | undefined> {
+        try {
+            const children = await this.#db.getData(this.#child_key);
+            return children.find((child: Child) => child.name === childName);
+        } catch (err) {
+            return undefined;
+        }
+    }
 }
