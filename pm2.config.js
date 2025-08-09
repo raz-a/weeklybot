ef6c980534ffff409fd07b103378d458f78d0aaa
Raz <raz-@outlook.com>
2025-08-09 06:38:39 +0000

    PM2

---
+++ pm2.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [{
+    script: 'weeklybot/app.js'
+  }],
+  deploy: {
+    production: {
+      'user' : 'raz',
+      'host' : '192.168.68.77',
+      'ref'  : 'origin/main',
+      'repo' : 'git@github.com:raz-a/weeklybot.git',
+      'path' : '/home/raz/weeklybot',
+      'post-deploy': 'npm install && npm run build'
+    }
+  }
+};
