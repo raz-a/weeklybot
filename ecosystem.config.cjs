cbb601e21e92677151135c33f2f168e14faa7a1e
Raz <raz-@outlook.com>
2025-08-09 00:23:53 -0700

    more fixup

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
       'ref'  : 'origin/main',
       'repo' : 'git@github.com:raz-a/weeklybot.git',
       'path' : '/home/raz/weeklybot',
-      'post-deploy': 'npm install && npm run build'
+      'post-deploy': 'npm install && npm run build pm2 reload ecosystem.config.cjs --env production'
     }
   }
 };
